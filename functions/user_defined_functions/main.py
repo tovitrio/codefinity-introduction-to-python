2a617bd7c60c5a35467a17efcb7b782aaea89e89
tovitrio <>
2026-03-10 14:50:00 +0000

    User-defined functions

---
+++ functions/user_defined_functions/main.py
@@ -0,0 +1,10 @@
+# Call the function and print the result
+
+def calculate_total_cost(price, quantity):
+    total_cost = price * quantity
+    return total_cost
+
+apples_total_cost = calculate_total_cost(1.50, 10)
+
+print(f"The total cost for apples is $", apples_total_cost)
+
